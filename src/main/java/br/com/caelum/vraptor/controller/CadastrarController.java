2e708af59a17f649a0826a2e071fe7bf68ac19a8
wevertonwos08 <weverton_brasilia@hotmail.com>
2021-05-29 19:04:36 -0300

    criação dos controladores cadastra,login e dasboard.implementação das paginas jsp

---
+++ src/main/java/br/com/caelum/vraptor/controller/CadastrarController.java
@@ -0,0 +1,21 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+
+@Path("cadastrar")
+@Controller	
+public class CadastrarController {
+
+	@Get("")
+	public void cadastrar() {
+		
+		
+		
+	}
+
+	
+
+}
---
+++ src/main/java/br/com/caelum/vraptor/controller/LoginController.java
@@ -0,0 +1,21 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+
+@Path("login")//quando acessar no navegador ,colocando login sera acessado pelo esse navegador
+@Controller
+public class LoginController {
+
+	@Get("")
+	public void login() {
+		
+		
+		
+	} 
+	
+	
+	
+}
